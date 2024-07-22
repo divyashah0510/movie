c5e0ca0c4790b1635b14fd66af9dcc0ead9f08f3
Divya Shah <102017379+divyashah0510@users.noreply.github.com>
2024-07-22 19:42:40 +0530

    feat: Initial commit

---
+++ web/assets/js/global.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Add event on mulitple elements
+
+const addEventOnElments = (elements, eventType, callback) => {
+    for (const elem of elements) elem.addEventListener(eventType, callback);
+};
+
+
+// Toggle search on mobile devices || small screen
+
+
+const searchBox = document.querySelector("[search-box]");
+const searchToggler = document.querySelectorAll("[search-toggler]");
+
+addEventOnElments(searchToggler,"click",()=>{
+    searchBox.classList.toggle("active");
+})
